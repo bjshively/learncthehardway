44f4e580151e42aa20ba455960d786f235f06aa4
Bradley Shively <bshively@gmail.com>
2018-11-03 01:07:08 -0400

    Exercise02: Build with makefiles

---
+++ exercise02/ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+	Multiline
+	Comment
+*/
+
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// Another comment
+	printf("You are %d miles away\n", distance);
+
+	printf("Let's print some more stuff.\n");
+	printf("And some more.\n");
+
+	return 0;
+}
+ 
